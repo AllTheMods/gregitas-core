97725cefdcb8513f71d22628c7b27f83c688d1e4
thevortex <james.g.bowley@gmail.com>
2024-01-15 05:15:12 -0800

    allow for aqueducts to carry water or lava (#4)
    * allow for aqueducts to carry water or lava
    
    * Update FluiDuct.java
    
    update mixin to block the lava dupe

---
+++ src/main/java/com/allthemods/gravitas2/core/mixin/FluiDuct.java
@@ -0,0 +1,38 @@
+package com.allthemods.gravitas2.core.mixin;
+
+
+import net.dries007.tfc.common.blocks.TFCBlockStateProperties;
+import net.dries007.tfc.common.blocks.rock.AqueductBlock;
+import net.dries007.tfc.common.fluids.FluidProperty;
+import net.dries007.tfc.common.fluids.IFluidLoggable;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Fluids;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = AqueductBlock.class, remap = false)
+public abstract class FluiDuct extends HorizontalDirectionalBlock implements IFluidLoggable {
+
+    @Shadow
+    @Final
+    public static final FluidProperty FLUID = TFCBlockStateProperties.WATER_AND_LAVA;
+
+
+    protected FluiDuct(Properties properties) {
+        super(properties);
+    }
+    @Inject(method = "pickupBlock(Lnet/minecraft/world/level/LevelAccessor;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)Lnet/minecraft/world/item/ItemStack;", cancellable = true, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/LevelAccessor;scheduleTick(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/Block;I)V"))
+    private void gregitas$cancelLavaPickup(LevelAccessor level, BlockPos pos, BlockState state, CallbackInfoReturnable<ItemStack> cir) {
+        if (state.getValue(getFluidProperty()).getFluid().isSame(Fluids.LAVA)){
+            cir.setReturnValue(ItemStack.EMPTY);
+        }
+    }
+}
