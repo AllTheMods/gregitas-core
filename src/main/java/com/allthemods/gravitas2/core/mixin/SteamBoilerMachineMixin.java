183c1c4fab3a344865eb82bb253ce759b1cb8a2c
thevortex <james.g.bowley@gmail.com>
2024-04-03 22:19:24 -0600

    adds support for TFC river water in steam boilers

---
+++ src/main/java/com/allthemods/gravitas2/core/mixin/SteamBoilerMachineMixin.java
@@ -0,0 +1,44 @@
+package com.allthemods.gravitas2.core.mixin;
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.At;
+
+import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
+import com.gregtechceu.gtceu.api.machine.feature.IExplosionMachine;
+import com.gregtechceu.gtceu.api.machine.feature.IUIMachine;
+import com.gregtechceu.gtceu.api.machine.steam.SteamBoilerMachine;
+import com.gregtechceu.gtceu.api.machine.steam.SteamWorkableMachine;
+import com.gregtechceu.gtceu.api.machine.trait.NotifiableFluidTank;
+import com.lowdragmc.lowdraglib.syncdata.annotation.Persisted;
+
+import net.dries007.tfc.common.fluids.TFCFluids;
+import net.minecraft.world.level.material.Fluids;
+
+
+@Mixin(value = SteamBoilerMachine.class, remap = false)
+public abstract class SteamBoilerMachineMixin extends SteamWorkableMachine implements IUIMachine, IExplosionMachine {
+    @Shadow
+    @Final
+    @Persisted
+    public NotifiableFluidTank waterTank;
+
+    public SteamBoilerMachineMixin(IMachineBlockEntity holder, boolean isHighPressure, Object[] args) {
+        
+        super(holder, isHighPressure, args);
+        
+        //TODO Auto-generated constructor stub
+    }
+   
+    @Inject(method = "<init>", at = @At("TAIL"), remap = false)
+    private void init(IMachineBlockEntity holder, boolean isHighPressure, Object[] args ,CallbackInfo ci) {
+        this.waterTank.setFilter(fluid -> ((TFCFluids.RIVER_WATER.get() == fluid.getFluid()) || (Fluids.WATER == fluid.getFluid())));
+    }
+ 
+    @Shadow
+    protected abstract NotifiableFluidTank createWaterTank(Object[] args);
+}
