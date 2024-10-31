7d22f4630bd69cdc599f4c5ab822e5278af3a502
YuHan Su <suyh4822@gmail.com>
2024-11-01 03:01:55 +0800

    patched up gtceu GUI issue

---
+++ src/main/java/com/allthemods/gravitas2/core/mixin/IDisplayUIMachineMixin.java
@@ -0,0 +1,43 @@
+package com.allthemods.gravitas2.core.mixin;
+
+import com.gregtechceu.gtceu.api.gui.GuiTextures;
+import com.gregtechceu.gtceu.api.gui.UITemplate;
+import com.gregtechceu.gtceu.api.machine.feature.IUIMachine;
+import com.gregtechceu.gtceu.api.machine.feature.multiblock.IDisplayUIMachine;
+import com.gregtechceu.gtceu.api.machine.feature.multiblock.IMultiController;
+import com.lowdragmc.lowdraglib.gui.modular.ModularUI;
+import com.lowdragmc.lowdraglib.gui.texture.IGuiTexture;
+import com.lowdragmc.lowdraglib.gui.util.ClickData;
+import com.lowdragmc.lowdraglib.gui.widget.ComponentPanelWidget;
+import com.lowdragmc.lowdraglib.gui.widget.DraggableScrollableWidgetGroup;
+import com.lowdragmc.lowdraglib.gui.widget.LabelWidget;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.List;
+
+
+@Mixin(value = IDisplayUIMachine.class, remap = false)
+public interface IDisplayUIMachineMixin extends IUIMachine, IMultiController {
+    @Shadow IGuiTexture getScreenTexture();
+    @Shadow void addDisplayText(List<Component> textList);
+    @Shadow void handleDisplayClick(String componentData, ClickData clickData);
+
+    @Overwrite
+    default ModularUI createUI(Player entityPlayer) {
+        var screen = new DraggableScrollableWidgetGroup(7, 4, 162, 121).setBackground(getScreenTexture());
+        screen.addWidget(new LabelWidget(4, 5, self().getBlockState().getBlock().getDescriptionId()));
+        screen.addWidget(new ComponentPanelWidget(4, 17, this::addDisplayText)
+                .textSupplier(this.self().getLevel().isClientSide ? null : this::addDisplayText)
+                .setMaxWidthLimit(150)
+                .clickHandler(this::handleDisplayClick));
+        return new ModularUI(176, 216, this, entityPlayer)
+                .background(GuiTextures.BACKGROUND)
+                .widget(screen)
+                .widget(UITemplate.bindPlayerInventory(entityPlayer.getInventory(), GuiTextures.SLOT, 7, 134, true));
+    }
+
+}
