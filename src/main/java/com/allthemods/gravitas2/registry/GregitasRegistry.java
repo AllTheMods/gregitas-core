93feb084e7d7f3b784b6daed983164a69de70172
thevortex <james.g.bowley@gmail.com>
2024-11-12 20:47:43 -0700

    add freeze-dried food trait

--- src/main/java/com/allthemods/gravitas2/registry/GregitasRegistry.java
+++ src/main/java/com/allthemods/gravitas2/registry/GregitasRegistry.java
@@ -3,7 +3,11 @@ package com.allthemods.gravitas2.registry;
 import com.allthemods.gravitas2.GregitasCore;
 import com.gregtechceu.gtceu.api.registry.registrate.GTRegistrate;
 
+import net.dries007.tfc.common.capabilities.food.FoodTrait;
+import net.minecraft.resources.ResourceLocation;
+
 public class GregitasRegistry {
     public static GTRegistrate GREGITAS_REGISTRATE = GTRegistrate.create(GregitasCore.MOD_ID);
 
+    public static final FoodTrait FreezeDried = FoodTrait.register(new ResourceLocation(GregitasCore.MOD_ID,"freeze_dried"),new FoodTrait(0.01F,"gregitas_core.freeze_dried"));
 }
